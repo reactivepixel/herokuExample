11651a6bfd808addc1a750761604921b97d5a7e8
Chris Chapman <cchapman@chapman-mbp.fullsail.com>
2016-06-11 22:52:20 -0400

    end of lecture

---
+++ test/__users.js
@@ -0,0 +1,63 @@
+const expect = require('chai').expect;
+const user = require('../src/models/user');
+const util = require('../lib/util');
+
+let testUser = {};
+
+describe('User Model', () => {
+
+  before(() => {
+    const mockUser = {
+      name: 'Cpt. Hydra',
+      age: 13,
+      hobby: 'Hailing',
+    };
+
+    user.create(mockUser, (error) => {
+      util.debug('Error creating mock user.', error);
+    }, (newDbUser) => {
+      testUser = newDbUser;
+    });
+  });
+
+  it('Should be able to Read All Users', (done) => {
+    user.findAll( (error) => {
+      util.debug('Error reading all Users', error);
+    }, (allUsers) => {
+      expect(allUsers.length).to.be.above(1);
+      done();
+    })
+  });
+
+  it('Should be able to Read One User', (done) => {
+    user.find(testUser, (error) => util.debug('Error reading One User', error),
+    (oneUser) => {
+        expect(oneUser.id).to.be.equal(testUser.id);
+        done();
+    })
+  });
+
+  it('Should be able to Create', () => {
+    expect(testUser.id).to.not.be.null;
+  });
+
+  it('Should be able to Update', (done) => {
+    const updateInfo = {
+      id: testUser.id,
+      name: 'Kevin',
+    }
+    user.update(updateInfo, (err) => util.debug('User failed to update', err),
+    (updatedDbUser) => {
+      expect(updatedDbUser.name).to.be.equal(updateInfo.name);
+      testUser = updatedDbUser;
+      done();
+    })
+  });
+  it('Should be able to Delete', (done) => {
+    user.destroy(testUser, (err) => util.debug('User Errored while destroying', err),
+    (responseFromDestroy) => {
+      expect(responseFromDestroy).to.be.equal(1);
+      done();
+    });
+  });
+});
